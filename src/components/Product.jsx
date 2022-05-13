cdb763baf29fef3a428299a130e6451dd3a1378e
brtauhid <br.tauhid@gmail.com>
2022-05-13 14:33:36 +0600

    setting up single product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 const Container = styled.div``;
 
-const Product = () => {
-  return <Container></Container>;
+const Product = ({ product }) => {
+  return <Container>This is product {product.img}</Container>;
 };
 
 export default Product;
