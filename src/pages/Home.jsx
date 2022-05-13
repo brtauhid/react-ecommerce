da018e1297bab2562d2cd646c952b4b5948f129a
brtauhid <br.tauhid@gmail.com>
2022-05-13 20:35:52 +0600

    newsletter section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Anouncements from "../components/Anouncements";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import NewsLetter from "../components/NewsLetter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider></Slider>
       <Categories></Categories>
       <Products></Products>
+      <NewsLetter></NewsLetter>
     </div>
   );
 };
