d6fb05521fb6930669a4886424112930d246655a
brtauhid <br.tauhid@gmail.com>
2022-05-13 14:12:44 +0600

    category area image positioning

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -3,13 +3,24 @@ const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
+  position: relative;
 `;
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
-const Info = styled.div``;
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 const Title = styled.h1``;
 const Button = styled.button``;
 
